3f81f366d026447cdabc07437752ed9c0ce58d17
Andrija <andrija.bbgdj@gmail.com>
2026-04-06 17:42:48 +0200

    Added notebooks 7-9

--- .ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-data_path = "\DepMap\"
-
-rna_expression = pd.read_csv("data_path" + "Expression_Public_25Q3_subsetted", index_col = 0)
-rna_expression.shape
